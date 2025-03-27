08d78abfc78642fd9de837805e469e8fa76dae92
Aaron Topping <aaronjtopping@gmail.com>
2025-03-27 00:30:14 -0700

    Fixed test causing error - re-deploying.

--- tests/test_smokeTest.py
+++ tests/test_smokeTest.py
@@ -9,15 +9,18 @@ from selenium.webdriver.support import expected_conditions
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
+from selenium.webdriver.chrome.options import Options
 
 class TestSmokeTest():
   def setup_method(self, method):
-    self.driver = webdriver.Firefox()
+    options = Options()
+    options.add_argument("--headless=new")
+    self.driver = webdriver.Chrome(options=options)
     self.vars = {}
-  
+
   def teardown_method(self, method):
     self.driver.quit()
-  
+
   def test_adminPage(self):
     self.driver.get("https://thegeneticsguy.github.io/cse270-teton/admin.html")
     elements = self.driver.find_elements(By.ID, "username")
@@ -26,7 +29,7 @@ class TestSmokeTest():
     self.driver.find_element(By.ID, "password").send_keys("qwerty")
     self.driver.find_element(By.CSS_SELECTOR, ".mysubmit:nth-child(4)").click()
     WebDriverWait(self.driver, 30).until(expected_conditions.text_to_be_present_in_element((By.CSS_SELECTOR, ".errorMessage"), "Invalid username and password."))
-  
+
   def test_directoryPage(self):
     self.driver.get("https://thegeneticsguy.github.io/cse270-teton/directory.html")
     self.driver.set_window_size(1200, 895)
@@ -34,7 +37,7 @@ class TestSmokeTest():
     assert self.driver.find_element(By.CSS_SELECTOR, ".gold-member:nth-child(9) > p:nth-child(2)").text == "Teton Turf and Tree"
     self.driver.find_element(By.ID, "directory-list").click()
     assert self.driver.find_element(By.CSS_SELECTOR, ".gold-member:nth-child(9) > p:nth-child(2)").text == "Teton Turf and Tree"
-  
+
   def test_homePage(self):
     self.driver.get("https://thegeneticsguy.github.io/cse270-teton/index.html")
     elements = self.driver.find_elements(By.CSS_SELECTOR, ".header-logo img")
@@ -51,9 +54,8 @@ class TestSmokeTest():
     elements = self.driver.find_elements(By.LINK_TEXT, "Join Us!")
     assert len(elements) > 0
     self.driver.find_element(By.LINK_TEXT, "Join Us!").click()
-    elements = self.driver.find_elements(By.NAME, "fname")
-    assert len(elements) > 0
-  
+    WebDriverWait(self.driver, 30).until(expected_conditions.presence_of_element_located((By.NAME, "fname")))
+
   def test_joinPage(self):
     self.driver.get("https://thegeneticsguy.github.io/cse270-teton/join.html")
     elements = self.driver.find_elements(By.NAME, "fname")
@@ -63,6 +65,5 @@ class TestSmokeTest():
     self.driver.find_element(By.NAME, "bizname").send_keys("Augmented Dynamics")
     self.driver.find_element(By.NAME, "biztitle").send_keys("CEO")
     self.driver.find_element(By.NAME, "submit").click()
-    elements = self.driver.find_elements(By.NAME, "email")
-    assert len(elements) > 0
-  
+    WebDriverWait(self.driver, 30).until(expected_conditions.presence_of_element_located((By.NAME, "email")))
+
